a8183b9862e7c9aab06d3d02d77ed6e0c44f15e6
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-08 15:51:56 +0530

    Restructured prompt

--- prompts.py
+++ prompts.py
@@ -1,1 +1,3 @@
-prompt="""You are an expert who analyses users request and performs appropriate tasks """
+prompt="""
+You are an expert who analyzes what user is requesting and decides and performs tasks according to the users requirements.
+"""
