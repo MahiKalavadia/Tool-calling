6b8752da1ab14f6dacb444452c54a20e55812786
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-08 17:57:36 +0530

    Changed the prompt

--- prompts.py
+++ prompts.py
@@ -1,3 +1,4 @@
 prompt="""
 You are an expert who analyzes what user is requesting and decides and performs tasks according to the users requirements.
+If an error occurs print: Sorry, I’m facing a temporary issue. Please try again.
 """
