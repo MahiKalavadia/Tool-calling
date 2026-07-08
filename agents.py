94d6a6c2f828d90b3807d72a1f668bf834884d4b
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-08 18:30:04 +0530

    Added the tools

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from tools import note_manage ,task_manage, task_status
+from tools import note_add, task_add, note_view, task_view, task_status
 from langchain_groq import ChatGroq
 from prompts import prompt
 from dotenv import load_dotenv
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 model=ChatGroq(model="llama-3.3-70b-versatile")
 
-tools = [note_manage, task_manage, task_status]
+tools = [note_view, note_add, task_view, task_add, task_status]
 logger.info(f"Tools available: {tools}")
 agent = create_agent(
     model=model,
