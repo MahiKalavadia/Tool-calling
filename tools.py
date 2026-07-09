0261e4a3651ea7d859efb16468cab61507f48eef
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-09 12:32:09 +0530

    Changed the return format

--- tools.py
+++ tools.py
@@ -1,43 +1,48 @@
 from langchain_core.tools import tool
 import json, logging, os
-from schema import NoteSchema, TaskSchema
+from schema import NoteSchema, TaskSchema, TaskViewSchema, NoteViewSchema, Note
 
 logger = logging.getLogger(__name__)
 
-NOTES_DIR = "notes.json"
-TASKS_DIR = "tasks.json"
+NOTES_FILES = "notes.json"
+TASKS_FILES = "tasks.json"
 
 def load_notes():
-    if not os.path.exists(NOTES_DIR):
+    if not os.path.exists(NOTES_FILES):
         return []
-    
-    with open(NOTES_DIR,"r") as f:
-        content = f.read().strip()
-        if not content:
-            return []
-        return json.loads(content)
+    try:
+        with open(NOTES_FILES,"r") as f:
+            content = f.read().strip()
+            if not content:
+                return []
+            return json.loads(content)
+    except json.JSONDecodeError as e:
+        logger.warning(f"Failed to load json {e}")
     
 def save_notes(notes):
-    with open(NOTES_DIR,"w") as f:
+    with open(NOTES_FILES,"w") as f:
         json.dump(notes, f, indent=4)
 
 def load_tasks():
-    if not os.path.exists(TASKS_DIR):
+    if not os.path.exists(TASKS_FILES):
        return []
+    try:
+        with open(TASKS_FILES,"r") as f:
+            content = f.read().strip()
+            if not content:
+                return []
+            return json.loads(content)
+    except json.JSONDecodeError as e:
+        logger.warning(f"Failed to load json {e}")
 
-    with open(TASKS_DIR,"r") as f:
-        content = f.read().strip()
-        if not content:
-            return []
-        return json.loads(content)
     
 def save_task(tasks):
-    with open(TASKS_DIR,"w") as f:
+    with open(TASKS_FILES,"w") as f:
         json.dump(tasks, f, indent=4)
 
 @tool(args_schema=NoteSchema)
 def note_add(note:str) -> str:
-    """Add the notes"""
+    """Only Add the notes"""
     try:
         logger.info("note_add tool enabled")
         notes = load_notes()
@@ -48,17 +53,19 @@ def note_add(note:str) -> str:
         })
         save_notes(notes)
         logger.info("Note added successfully")
-        return "Note saved successfully."
+        return {
+            "message":"Note saved successfully!"
+        }
     except FileNotFoundError as e:
-        logger.warning(f"Note file not found: {e}")
+        logger.exception(f"Note file not found: {e}")
         return "Note file not found."
     except Exception as e:
-        logger.warning(f"Failed to add note: {e}")
+        logger.exception(f"Failed to add note: {e}")
         return "An error occurred while saving the note."
 
-@tool
-def note_view() -> str:
-    """View the notes"""
+@tool()
+def note_view(_:str="") -> dict:
+    """Only View the notes"""
     try:
         logger.info("note_view tool enabled")
         notes = load_notes()
@@ -68,17 +75,19 @@ def note_view() -> str:
             return "No notes found."
         formatted_notes = "\n".join(f"{i+1}. {note['note']}" for i, note in enumerate(notes))
         logger.info("Notes retrieved successfully")
-        return formatted_notes
+        return {
+            "note": notes
+        }
     except FileNotFoundError as e:
-        logger.warning(f"Note file not found: {e}")
+        logger.exception(f"Note file not found: {e}")
         return "Note file not found."
     except Exception as e:
-        logger.warning(f"Failed to view notes: {e}")
+        logger.exception(f"Failed to view notes: {e}")
         return "An error occurred while retrieving notes."
 
 @tool(args_schema=TaskSchema)
 def task_add(task:str) -> str:
-    """Add the task"""
+    """Only Add the task"""
     try:
         logger.info("task_add tool enabled")
         tasks = load_tasks()
@@ -90,17 +99,19 @@ def task_add(task:str) -> str:
         })
         save_task(tasks)
         logger.info("Task added successfully")
-        return "Task added successfully."
+        return {
+            "message":"Task saved successfully!"
+        }
     except FileNotFoundError as e:
-        logger.warning(f"Task file not found: {e}")
+        logger.exception(f"Task file not found: {e}")
         return "Task file not found."
     except Exception as e:
         logger.exception(f"Failed to add task: {e}")
         return "An error occurred while saving the task."
 
-@tool
-def task_view(_:str="") -> str:
-    """View the task"""
+@tool()
+def task_view(_:str="") -> dict:
+    """Only view the task"""
     try:
         logger.info("task_view tool enabled")
         tasks = load_tasks()
@@ -108,18 +119,20 @@ def task_view(_:str="") -> str:
         if not tasks:
             logger.info("No tasks found")
             return "No tasks found."
-        formatted_tasks = "\n".join(f"{i+1}. {task['task']}" for i, task in enumerate(tasks))
+        # formatted_tasks = "\n".join(f"{i+1}. {task['task']} - {task['status']}" for i, task in enumerate(tasks))
         logger.info("Tasks retrieved successfully")
-        return formatted_tasks
+        return {
+            "task":tasks
+        }
     except FileNotFoundError as e:
-        logger.warning(f"Task file not found: {e}")
+        logger.exception(f"Task file not found: {e}")
         return "Task file not found."
     except Exception as e:
         logger.exception(f"Failed to view tasks: {e}")
         return "An error occurred while retrieving tasks."
 
 @tool(args_schema=TaskSchema)
-def task_status(task:str) -> str:
+def change_task_status(task:str) -> str:
     """Match the task user entered with the task available in file and change its status"""
     try:
         logger.info(f"task_status tool enabled for task: {task}")
@@ -129,7 +142,11 @@ def task_status(task:str) -> str:
                 t["status"] = "completed"
                 save_task(tasks)
                 logger.info(f"Task status updated for task id: {t['id']}")
-                return f"Task '{t['task']}' marked as completed."
+                return {
+                    "status":"completed",
+                    "task":t["task"],
+                    "status":t["status"]
+                }
         logger.info(f"No matching task found for: {task}")
         return "No matching task found."
     except Exception as e:
