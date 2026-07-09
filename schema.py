1b20c75c172b918be524e8ea9223da95603946bb
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-09 12:33:42 +0530

    Added schema for chat reuqest and response

--- schema.py
+++ schema.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional, List
 
 
 class NoteSchema(BaseModel):
@@ -7,3 +7,30 @@ class NoteSchema(BaseModel):
 
 class TaskSchema(BaseModel):
     task: str
+
+class ChatRequest(BaseModel):
+    query: str
+
+class ChatResponse(BaseModel):
+    response: str
+
+class Note(BaseModel):
+    id: int
+    note: str
+
+class NoteViewSchema(BaseModel):
+    note: List[Note]
+
+class Task(BaseModel):
+    id: int
+    task: str
+    status: str
+
+class TaskViewSchema(BaseModel):
+    task: List[Task]
+
+class abc(BaseModel):
+    id: Optional[int] = Field(None, description="The ID of the item")
+    note: Optional[str] = Field(None, description="The note content")
+    task: Optional[str] = Field(None, description="The task content")
+    status: Optional[str] = Field(None, description="The status of the task")
