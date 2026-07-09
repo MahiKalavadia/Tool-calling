247abe1c6e390d12ddd9fec5c4ac520dff8fbfc3
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-09 12:34:02 +0530

    changed the prompt

--- prompts.py
+++ prompts.py
@@ -12,5 +12,9 @@ INSTRUCTIONS:
    - task_status: Mark a task as completed
 
 4. Call the appropriate tools based on user requiremenets.
-5. Always present the actual content and results returned by the tool like lists of notes or tasks, details in the final response.
+6. If you encounter any error always return “Sorry, I’m facing a temporary issue. Please try again.”
+7. Always return response in json format.
+8. Always present the actual content and results returned by the tool like lists of notes or tasks, details in the final response in json format.
+9. Never call any tool if the user request is not clear or ambiguous. Instead, ask the user for clarification.
+10. Never call any extra tools that are not required for the user request. Only call the tools that are necessary to fulfill the user request.
 """
